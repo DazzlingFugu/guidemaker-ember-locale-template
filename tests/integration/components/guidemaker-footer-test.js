d17f72c0099faadc759a6ec3b9cbf2c8a6a3ff92
Marine Dunstetter <bluecut.official@gmail.com>
2023-10-20 14:16:06 +0200

    replace the default integration test for guidemaker footer

--- tests/integration/components/guidemaker-footer-test.js
+++ tests/integration/components/guidemaker-footer-test.js
@@ -6,21 +6,8 @@ import { hbs } from 'ember-cli-htmlbars';
 module('Integration | Component | guidemaker-footer', function (hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders', async function (assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
-
+  test('it renders as a footer tag', async function (assert) {
     await render(hbs`<GuidemakerFooter />`);
-
-    assert.dom(this.element).hasText('');
-
-    // Template block usage:
-    await render(hbs`
-      <GuidemakerFooter>
-        template block text
-      </GuidemakerFooter>
-    `);
-
-    assert.dom(this.element).hasText('template block text');
+    assert.dom('footer').exists();
   });
 });
