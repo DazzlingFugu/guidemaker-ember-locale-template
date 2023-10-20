0af4402b224a69dff7d1a4a4b3fa3dabef992ea5
Marine Dunstetter <bluecut.official@gmail.com>
2023-10-20 12:18:58 +0200

    add a config for search placeholder

--- addon/components/search-input.js
+++ addon/components/search-input.js
@@ -1,18 +1,15 @@
-import { getOwner } from '@ember/application';
 import { task, timeout } from 'ember-concurrency';
 import { action } from '@ember/object';
 import { later } from '@ember/runloop';
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
 import algoliasearch from 'algoliasearch';
+import config from 'ember-get-config';
 
 const SEARCH_DEBOUNCE_PERIOD = 300;
 
 export default class SearchInputComponent extends Component {
-<<<<<<< HEAD
-=======
   config = config;
->>>>>>> f506033 (fixup! use native classes for search-input component)
   value = '';
 
   @tracked response = undefined;
@@ -20,19 +17,19 @@ export default class SearchInputComponent extends Component {
   _focused = false;
   _resultTetherConstraints = [{ to: 'window', pin: ['left', 'right'] }];
 
-  constructor(owner, args) {
-    super(owner, args);
-    const config = getOwner(this).resolveRegistration('config:environment');
+  constructor() {
+    super(...arguments);
     const { algoliaId, algoliaKey, indexName } = config['algolia'] || {};
 
     if (algoliaId && algoliaKey && indexName) {
       this.searchFunction = true;
       this.client = algoliasearch(algoliaId, algoliaKey);
       this.index = this.client.initIndex(indexName);
+      this.strSearchPlaceholder = config.guidemaker.texts.searchPlaceholder;
     }
   }
 
-  search = task(function* (query) {
+  @task({ restartable: true }) *search(query) {
     yield timeout(SEARCH_DEBOUNCE_PERIOD);
 
     if (!query) {
@@ -46,7 +43,7 @@ export default class SearchInputComponent extends Component {
 
     this.response = yield this.index.search(query, searchObj);
     return this.response;
-  }).restartable();
+  }
 
   @action
   oninput(value) {
